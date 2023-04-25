75f521f3c47230a14aa4bc721c90b623a1c7c23c
Bismark Agyare <bismarkagyare54@gmail.com>
2023-04-25 18:17:02 +0000

    setup eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
+};
