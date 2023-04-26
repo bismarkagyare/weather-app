dbdea5710966d701fa07c964b6356bb50aca2de7
Bismark Agyare <bismarkagyare54@gmail.com>
2023-04-26 10:45:07 +0000

    target api fuctions for testing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+async function checkWeather() {
+  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric';
+  const apiKey = '64517a1e83aa07c580b750e305e6772f';
+
+  const response = await fetch(`${apiUrl}&appid=${apiKey}`);
+  const data = await response.json();
+  console.log(data);
+}
+
+checkWeather();
