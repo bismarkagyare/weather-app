7c941c453027afdba84a705707de6ed2ca048409
Bismark Agyare <bismarkagyare54@gmail.com>
2023-04-28 18:37:56 +0000

    refactor code

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,22 +1,22 @@
+import {
+  updateCityName,
+  updateDegree,
+  updateWeatherImage,
+  updateFeelsLike,
+  updateChanceOfRain,
+  updateWindSpeed,
+  updateHumidity,
+} from './dom';
+
 const weatherImageMap = {
   Clear: 'sunny-w.svg',
-  Clouds: 'clouds.png',
-  Rain: 'rain.png',
-  Mist: 'mist.png',
+  Clouds: 'cloudy.svg',
+  Rain: 'rainy.svg',
+  Mist: 'mist.svg',
   Drizzle: 'drizzle.png',
-  Snow: 'snow.png',
-  Haze: 'haze.png',
+  Snow: 'snow.svg',
+  Haze: 'haze.svg',
 };
-
-const cityName = document.querySelector('.city-name');
-const degree = document.querySelector('.degree');
-const weatherImage = document.querySelector('.weather-icon');
-
-// air conditions
-const feelsLike = document.querySelector('.real-feel h3');
-const chanceOfRain = document.querySelector('.chance-of-rain h3');
-const windSpeed = document.querySelector('.wind h3');
-const humidity = document.querySelector('.humidity h3');
 
 async function checkWeather(city) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
@@ -28,17 +28,15 @@ async function checkWeather(city) {
     const data = await response.json();
     console.log(data);
 
-    cityName.textContent = data.name;
-    degree.textContent = `${Math.round(data.main.temp)} °C`;
-
-    // air conditions
-    feelsLike.textContent = `${Math.round(data.main.feels_like)} °C`;
-    windSpeed.textContent = `${data.wind.speed} km/h`;
-    humidity.textContent = `${data.main.humidity} %`;
+    updateCityName(data.name);
+    updateDegree(data.main.temp);
+    updateFeelsLike(data.main.feels_like);
+    updateWindSpeed(data.wind.speed);
+    updateHumidity(data.main.humidity);
 
     const weatherCondition = data.weather[0].main;
     const weatherImageFileName = weatherImageMap[weatherCondition];
-    weatherImage.src = `images/${weatherImageFileName}`;
+    updateWeatherImage(weatherImageFileName);
   } catch (error) {
     alert(error);
   }
