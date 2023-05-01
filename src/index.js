cced7b62a32567aebe676f00eb37ebf13b3305a7
Bismark Agyare <bismarkagyare54@gmail.com>
2023-05-01 23:51:24 +0000

    comments out the getCurrentTime func

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import checkWeather from './modules/api';
+//import { getCurrentDateTime } from './modules/date-time';
 
 document.addEventListener('DOMContentLoaded', () => {
   const searchInput = document.querySelector('.search-container input');
   const searchBtn = document.querySelector('.submit');
   checkWeather('Manchester');
+  //getCurrentDateTime();
 
   const search = () => {
     if (searchInput.value === '') return;
